3bb59a678cb5d6cea54693f23e5bc11f33969d59
Antony Maia <antonymaiati@gmail.com>
2021-02-15 12:21:31 -0300

    showing customer data and image for profile page

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { API_CONFIG } from '../../config/api.config';
+import { ClienteDTO } from '../../models/cliente.dto';
+import { ClienteService } from '../../services/domain/cliente.service';
 import { StorageService } from '../../services/storage.service';
 
 /**
@@ -16,18 +19,38 @@ import { StorageService } from '../../services/storage.service';
 })
 export class ProfilePage {
 
+  cliente: ClienteDTO;
+
   email : string;
 
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
-              public storage: StorageService) {
+              public storage: StorageService,
+              public clienteService : ClienteService ) {
   }
 
   ionViewDidLoad() {
     let localUser = this.storage.getLocalUser();
     if(localUser && localUser.email){
-      this.email = localUser.email;
+      this.clienteService.findByEmail(localUser.email)
+        .subscribe(response =>{
+          this.cliente = response;
+          this.getImageIfExists();
+        },
+        error =>{
+
+        });
     }
+  }
+
+  getImageIfExists(){
+    this.clienteService.getImageFromBucket(this.cliente.id)
+      .subscribe(response =>{
+        this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`;
+      },
+      error =>{
+
+      })
   }
 
 }
